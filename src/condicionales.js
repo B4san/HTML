178b1d32bd600b201013bece15b7417295f6d3a4
B4san <b4san.34@gmail.com>
2024-03-15 10:08:14 -0600

    condicionales, version 2

--- src/condicionales.js
+++ src/condicionales.js
@@ -1,39 +1,56 @@
-var mensaje = "batir las fresas"
-var hay_fresas;
+var fresas;
+var Peras;
+var mangos;
+var mensaje;
+
 
 function jugo_de_fresas() {
     window.alert(mensaje);
-    document.querySelector('#headher').innerHTML = mensaje
+    document.querySelector('#header').innerHTML = mensaje;
   }
-
   function servir_vaso_de_agua() {
     window.alert(mensaje);
-    document.querySelector('#headher').innerHTML = mensaje
+    document.querySelector('#header').innerHTML = mensaje;
   }
-
   function jugo_de_peras() {
     window.alert(mensaje);
-    document.querySelector('#headher').innerHTML = mensaje
+    document.querySelector('#header').innerHTML = mensaje;
   }
+  function jugo_de_mango() {
+    window.alert(mensaje);
+    document.querySelector('#header').innerHTML = mensaje;
+  }
+ 
+mangos = false;
+Peras = false;
+fresas = false;
 
-  function jugo_de_mangos() {
-    window.alert('Batir mangos');
-    document.querySelector('#headher').innerHTML = mensaje
-  }
+if (fresas) {
+ mensaje = "Batir fresas"
+  jugo_de_fresas();
+} else if (Peras) {
+mensaje = "Batir peras"
+  jugo_de_peras();
+} else if (mangos) {
+mensaje = "Batir mangos"
+  jugo_de_mango();
+} else {
+mensaje = "Servir agua"
+  servir_vaso_de_agua();
+}
 
-  hay_mangos = true;
-  hay_peras = false;
-  hay_fresas = false;
-  if (hay_fresas) {
-    mensaje = 'Batir fresas'
-    jugo_de_fresas();
-  } else if (hay_peras) {
-    mensaje = 'batir peras'
-    jugo_de_peras();
-  } else if (hay_mangos) {
-    mensaje = 'batir mangos'
-    jugo_de_mangos();
-  } else {
-    mensaje = 'servir vaso de agua'
-    servir_vaso_de_agua();
-  }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
