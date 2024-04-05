5aa1f03641baef71d96d9baab2f55738762e80cc
B4san <b4san.34@gmail.com>
2024-04-05 11:09:33 -0600

    cesta(version1)

---
+++ src/Cesta.js
@@ -0,0 +1,8 @@
+document.querySelector('#header').innerHTML = "estoy leyendo el documento Cesta.js"
+
+var Cesta
+
+var i = 0
+
+var respuesta = prompt("1.Ver cesta\n2. Añadir a cesta\n3. Quitar ultima fruta de cesta")
+    switch(parseInt(respuesta)){}
